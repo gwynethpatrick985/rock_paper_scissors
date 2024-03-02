c35a00c2d98214de10c79c21106d90ff08e045bf
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-03-02 16:05:55 -0700

    feat: Will add unfinished player round method, polishes the CPU round method to return results

--- src/game.js
+++ src/game.js
@@ -2,17 +2,36 @@ class game{
     constructor(player1 = new player('cpu_0'), player2 = new player ('cpu_1')){
         this.player1 = player1,
         this.player2 = player2,
-        this.classicMoves = ['rock','paper','scissors'],
-        console.log(this.player1, 'game scope');
+        this.classicMoves = ['rock','paper','scissors'];
+      
     };
     newCPURound(moveSet){
-        console.log(this.player1, 'new cpu round');
+       let result = ''
        let move1 = this.player1.randomMove(moveSet);
-       console.log(move1, 'game scope');
        let move2 = this.player2.randomMove(moveSet);
-       console.log(move2, 'game scope');
        let winner = this.determineWinner(move1,move2)
-       console.log(winner, 'winner')
+       if (winner === move1){
+            this.player1.wins += 1
+            result = "Player 1 Wins!"
+       } else if (winner === move2){
+            this.player2.winner += 1
+            result =  "Player 2 Wins!"
+       } else {
+            result = "Draw!"
+       };
+       return result;
+    };
+    newPlayerRound(moveSet, playerMove){
+        let move1 = this.player1.takeTurn(playerMove);
+        let move2 = this.player2.takeTurn.randomMove(moveSet);
+        let winner = this.determineWinner(move1, move2);
+        if (winner === move1){
+            this.player1.wins += 1
+        } else if (winner === move2){
+            this.player2.winner += 1
+        } else {
+            return "Draw!"
+        };
     };
     determineWinner(move1, move2){
         if (!this.isDraw(move1,move2)){
@@ -29,12 +48,10 @@ class game{
                     return move1
                 };
             }
-            return move2
-  
-        } else {
-            return 'is draw'
-        };
+            return move2;
+        }
     };
+    
     isDraw(move1, move2){
         if(move1 === move2){
             return true
